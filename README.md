44c52b6f3ae9c8b4619ee0e9cee4e3b962133830
danny176 <danielfrederiksmose96@gmail.com>
2024-12-09 21:37:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MAK-Hjemmeside
+ Hjemmeside til Mohammad Personlig Træner. MDU Eksamen 2024 2. Semester
---
+++ script/globalScript.js
@@ -0,0 +1,32 @@
+//Navigation animation til links med GSAP og Splittext lib
+
+const staggerLinks = document.querySelectorAll(".stagger-link"); //Laver en variabed som vælger alle elementer der har .stagger-link class.
+
+staggerLinks.forEach((link) => {
+  const textElement = link.querySelector(".stagger-link-text"); //En NodeList der indeholder alle links med klassen .stagger-link. Bruges til at finde den relevante del af linket (den tekst, der skal animeres). Laver en varibel textElement, så de funde tekst elementer kan bruges i animationen
+
+  // Opdeler teksten til bogstaver der kan animeres
+  const splitText = new SplitType(textElement, { types: "chars" });
+
+  // Laver en GSAP timeline der skal styre animationen
+  const tl = gsap.timeline({ paused: true });
+
+  // Flytter teksten op af Y-aksen, og putter en stack effekt på bogstaverne
+  tl.to(splitText.chars, {
+    y: "-150%", // Flyt hver bogstav -150% op af Y-aksen
+    duration: 0.3,
+    stagger: 0.01, // Hvert bogstavs animation er forsinket med 0.1 sekund, så det giver en rullende effekt
+    ease: "power4.inOut", //Easing effekt
+    overwrite: true, //Effekten bliver startet forfra når du forlader hover stadiet og hurtigt bevæger dig tilbage
+  });
+
+  // Mouse enter event til at starte GSAP timeline animationen
+  link.addEventListener("mouseenter", () => {
+    tl.play();
+  });
+
+  // Mouse leave event afspiller GSAP timeline animationen baglæns
+  link.addEventListener("mouseleave", () => {
+    tl.reverse();
+  });
+});
