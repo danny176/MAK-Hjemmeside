33b910993546146f28b4b220ee4cf07f3a735586
danny176 <danielfrederiksmose96@gmail.com>
2024-12-16 18:21:42 +0100

    Mere kommentar på JS

--- script/globalScript.js
+++ script/globalScript.js
@@ -31,13 +31,25 @@ staggerLinks.forEach((link) => {
   });
 });
 
-//Hero billede parallax effect
-window.addEventListener("scroll", () => {
+// Parallax effekt for vores hero billeder
+function parallaxEffect() {
+  // Hent den nuværende scroll-position på siden
   const scrollPosition = window.scrollY;
+
+  // Vælg hero-billedet (billedet inde i .herobillede containeren)
   const herobillede = document.querySelector(".herobillede img");
-  const speed = 0.5; // Hastigheden
+
+  // Sæt hastigheden for parallax-effekten
+  const speed = 0.5; // Hastigheden af bevægelsen i forhold til scrollen
+
+  // Ændre billedets position baseret på scroll-positionen og hastigheden
   herobillede.style.transform = `translateY(${scrollPosition * speed}px)`;
-});
+}
+
+// Lyt efter scroll-begivenhed på vinduet og kør parallax-effekten, når der scroller
+window.addEventListener("scroll", parallaxEffect);
+
+//HAMBURGER MENU MED SLIDE
 
 // Opretter variabler fra HTML ved hjælp af deres klasser
 const menuButton = document.querySelector(".hambruger-menu-knap"); // Knap der åbner/lukker menuen
